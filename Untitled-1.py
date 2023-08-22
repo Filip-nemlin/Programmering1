f2dd562fa569e49bf15c61596fc4a758be7bb625
Filip-nemlin <filip.nemlin@elev.ntig.se>
2023-08-22 09:47:43 +0200

    hejhej

---
+++ Untitled-1.py
@@ -0,0 +1,17 @@
+print("hello world- jag är ny på programmering")
+print(20 - 20)
+print(20 * 20)
+print(20 / 20)
+print(10 / 3)
+print("10 // 3")
+
+x = input("namn: ")
+print(x)
+age = input("ålder: ")
+print(age)
+print("hej,", x, "du är", age, "år=")
+print("""
+hej klassen
+detta är flerradig print""") 
+
+print("resultat:\nTEINF22B vann")
